0ab45134cabf26167fbcb0321141102d5f418cbd
Kumar Gaurav <kumargauravgupta3@gmail.com>
2018-04-08 21:36:45 +0530

    Added ConnectionFailedException

---
+++ balancer/src/main/java/com/kgaurav/balancer/exception/ConnectionFailedException.java
@@ -0,0 +1,7 @@
+package com.kgaurav.balancer.exception;
+
+public class ConnectionFailedException extends Exception {
+    public ConnectionFailedException(String message) {
+        super(message);
+    }
+}
