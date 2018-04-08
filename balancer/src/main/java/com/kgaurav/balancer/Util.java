66c4bbee9d36c39cc40d2e06978b66466abd5136
Kumar Gaurav <kumargauravgupta3@gmail.com>
2018-04-08 21:36:06 +0530

    Added more utility methods

--- balancer/src/main/java/com/kgaurav/balancer/Util.java
+++ balancer/src/main/java/com/kgaurav/balancer/Util.java
@@ -1,12 +1,14 @@
 package com.kgaurav.balancer;
 
+import com.kgaurav.balancer.exception.ConnectionFailedException;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.math.BigInteger;
+import java.net.Socket;
 import java.util.Properties;
+import java.util.Random;
 
 /**
  * Created by admin on 4/5/2018.
@@ -37,5 +39,133 @@ public class Util {
         } catch (IOException e) {
             return false;
         }
+    }
+
+    public static String convertStreamToString(InputStream is) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                throw e;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Closes the {@link Socket}
+     * @param socket
+     */
+    public static void closeSocket(Socket socket) {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Closes given {@link InputStream}
+     * @param stream
+     */
+    public static void closeInputStream(InputStream stream) {
+        try {
+            stream.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Closes given {@link OutputStream}
+     * @param stream
+     */
+    public static void closeOutputStream(OutputStream stream) {
+        try {
+            stream.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Sends message to a particular address
+     * @param address
+     * @param port
+     * @param data
+     * @return true if sent successfully false otherwise
+     */
+    public static String  sendAndReceiveDataToNode(String address, int port, String data) throws ConnectionFailedException {
+        Socket me = null;
+        DataOutputStream outputStream = null;
+        DataInputStream inputStream = null;
+        String responseData = null;
+        try {
+            LOGGER.info("Connecting to destination");
+            me = new Socket(address, port);
+            LOGGER.info("Connected to destination");
+            outputStream = new DataOutputStream(me.getOutputStream());
+            outputStream.write(data.getBytes());
+            inputStream = new DataInputStream(me.getInputStream());
+            responseData = convertStreamToString(inputStream);
+            return responseData;
+        } catch (IOException e) {
+            LOGGER.error("Connection failed with balancer");
+            LOGGER.error(e.getMessage(), e);
+            throw new ConnectionFailedException(e.getMessage());
+        }
+        finally {
+            closeOutputStream(outputStream);
+            closeSocket(me);
+            closeInputStream(inputStream);
+        }
+    }
+
+    /**
+     * Return data to given socket
+     * @param socket
+     * @param data
+     * @return true if sent successfully false otherwise
+     */
+    public static boolean returnData (Socket socket, String data) throws ConnectionFailedException {
+        Socket me = socket;
+        DataOutputStream outputStream = null;
+        DataInputStream inputStream = null;
+        String responseData = null;
+        try {
+            LOGGER.info("Connecting to destination");
+            LOGGER.info("Connected to destination");
+            outputStream = new DataOutputStream(me.getOutputStream());
+            outputStream.write(data.getBytes());
+            return true;
+        } catch (IOException e) {
+            LOGGER.error("Connection failed with balancer");
+            LOGGER.error(e.getMessage(), e);
+            throw new ConnectionFailedException(e.getMessage());
+        }
+        finally {
+            closeOutputStream(outputStream);
+            closeSocket(me);
+            closeInputStream(inputStream);
+        }
+    }
+
+    public static String generateInternalKey() {
+        return new BigInteger(130, new Random()).toString(32);
+    }
+
+    public static boolean isNullOrEmpty(String string) {
+        return string == null || string.trim().length() < 1;
     }
 }
